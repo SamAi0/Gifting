ac7f429e2a725f46c627fac2879cf274b1a04d10
SamAi0 <ai1styear@gmail.com>
2026-04-29 18:19:03 +0530

    fix production env settings

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'django-insecure-f%+5lw^jvuu)r6ff2gy7sv7tu7)ga1@d=ehpdyg5%i1i#-@%m@')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', 'localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = ["gifting-backend-v5wr.onrender.com"]
 
 
 # Application definition
